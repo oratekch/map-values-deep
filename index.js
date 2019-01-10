f0397304674d1eafde18d74328945238a8e247a7
Aurélien Essig <aurelien.essig@oratek.ch>
2019-01-10 13:13:30 +0100

    Updated using deep-loadash

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-const mapValuesDeep = require('./fp');
+const _ = require('lodash');
 
-module.exports = (obj, cb) => mapValuesDeep(cb)(obj);
+const mapValuesDeep = function(object, callback, propertyPath){
+  propertyPath = propertyPath || '';
+  if(_.isArray(object)){
+      return _.map(object, deepMapValuesIteratee);
+  }
+  else if(_.isObject(object) && !_.isDate(object) && !_.isRegExp(object) && !_.isFunction(object)){
+      return _.extend({}, object, _.mapValues(object, deepMapValuesIteratee));
+  }
+  else{
+      return callback(object, propertyPath);
+  }
+
+  function deepMapValuesIteratee(value, key){
+      var valuePath = propertyPath ? propertyPath + '.' + key: key;
+      return mapValuesDeep(value, callback, valuePath);
+  }
+}
+
+module.exports = mapValuesDeep;
