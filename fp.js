798cb643ca3d1838d5603ef4506a5c87c4cc841a
aessig <auressig@gmail.com>
2019-01-10 12:42:11 +0100

    Update fp.js

--- fp.js
+++ fp.js
@@ -4,7 +4,7 @@ const mapValuesDeep = cb => obj =>
   Array.isArray(obj)
     ? obj.map(mapValuesDeep(cb))
     : isPlainObject(obj)
-      ? mapValues(mapValuesDeep(cb))(obj)
+      ? mapValues(mapValuesDeep(cb)(obj))
       : obj;
 
 module.exports = mapValuesDeep;
